1b31218e2309a61992160db1406dd8d3a519106a
TheyCallMeCheng <cheng-corp@protonmail.com>
2022-11-22 18:31:44 +0100

    Basic layout with tailwind

--- components/Job.tsx
+++ components/Job.tsx
@@ -2,15 +2,17 @@ import styles from '../styles/Job.module.css'
 
 export default function Job({title, company, salary, location, tags}){
     return (
-        <div className={styles.jobs}>
-            <h1> {title} </h1>
-            <p> <span>Company:</span> {company} </p>
-            <p> <span>Salary:</span> {salary ? salary : "N/A"} </p>
-            <p> <span>Location:</span> {location ? location : "N/A"} </p>
-            <p> {tags.join(' | ')} </p>
+        <div className='pb-4 text-xl md:flex md:items-center justify-between'>
             <div>
+                <h1 className='text-3xl font-bold'> {title} </h1>
+                <p> <span>Company:</span> {company} </p>
+                <p> <span>Salary:</span> {salary ? salary : "N/A"} </p>
+                <p> <span>Location:</span> {location ? location : "N/A"} </p>
+                <p> {tags.join(' | ')} </p>
+            </div>
+            <div className='font-bold text-center border-2 md:border-0 border-black rounded py-2 my-2 '>
                 <a>
-                   Apply →
+                   Apply
                 </a>
             </div>
         </div>
